6be010e4444967859bd196c72c6986429548c551
Pat Boon <boonie21371@gmail.com>
2025-02-21 13:42:39 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ClickMe from "@/components/ui/ClickMe";
+
+const App = () => {
+  return (
+    <div>
+      <ClickMe />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ui/ClickMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const ClickMe = () => {
+  return (
+    <div>
+      <Button>ClickMe</Button>
+    </div>
+  );
+};
+
+export default ClickMe;
